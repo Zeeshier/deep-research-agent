81982cc25e3e86292ebff9f8a4d059725ca26acb
Zeeshan Ahmad <Zeeshanwarraich51@gmail.com>
2025-08-10 20:24:53 +0500

    update error handling

--- src/tools/llm.py
+++ src/tools/llm.py
@@ -4,4 +4,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-llm = ChatGroq( model="meta-llama/llama-4-scout-17b-16e-instruct", temperature=0, api_key=os.getenv("GROQ_API_KEY"))
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+if not GROQ_API_KEY:
+    raise RuntimeError("Missing GROQ_API_KEY")
+
+llm = ChatGroq( model="meta-llama/llama-4-scout-17b-16e-instruct", temperature=0, api_key=GROQ_API_KEY)
